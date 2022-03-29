51c5a5d5e9cb0052dc2d1e53b08e484e5074d871
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2022-03-29 16:14:31 +0300

    init socket.io on server and client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,17 @@
+import io from 'socket.io-client';
+import { useEffect } from 'react';
+
+const SERVER_URL = 'http://localhost:8080';
+
 export const App = () => {
+  useEffect(() => {
+    const socket = io(SERVER_URL);
+
+    socket.on("connect", () => {
+      console.log(socket.id);
+    });
+  }, []);
+
   return (
     <div className="App">
       ZooMio Online Chat
--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
+const http = require('http');
 const express = require('express');
+const cors = require('cors');
+const { Server } = require('socket.io');
 
 const PORT = process.env.APP_PORT || 8080;
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: CLIENT_URL,
+    methods: ['GET', 'POST'],
+  },
+});
 
-app.listen(PORT, () => {
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
