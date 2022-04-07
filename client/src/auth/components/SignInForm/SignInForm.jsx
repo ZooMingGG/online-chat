190fcb287eac7c0cd5aff6d0d42b5f0ea1bb8b50
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2022-04-07 13:18:32 +0300

    create basic structure for sign in/up forms

--- client/src/auth/components/SignInForm/SignInForm.jsx
+++ client/src/auth/components/SignInForm/SignInForm.jsx
@@ -1,7 +1,47 @@
+import { useFormik } from 'formik';
+
 export const SignInForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      tag: '',
+      password: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div>
-      Sign In Form
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <div>
+          <label htmlFor="tag">Enter your tag</label>
+        </div>
+        <div>
+          <input
+            id="tag"
+            name="tag"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.tag}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <label htmlFor="password">Enter your password</label>
+        </div>
+        <div>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+        </div>
+      </div>
+      <button type="submit">Sign In</button>
+    </form>
   );
 };
--- client/src/auth/components/SignUpForm/SignUpForm.jsx
+++ client/src/auth/components/SignUpForm/SignUpForm.jsx
@@ -1,7 +1,77 @@
+import { useFormik } from 'formik';
+
 export const SignUpForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      tag: '',
+      password: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div>
-      Sign Up Form
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <div>
+          <label htmlFor="firstName">Enter your first name</label>
+        </div>
+        <div>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <label htmlFor="lastName">Enter your last name</label>
+        </div>
+        <div>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <label htmlFor="tag">Enter your tag</label>
+        </div>
+        <div>
+          <input
+            id="tag"
+            name="tag"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.tag}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <label htmlFor="password">Enter your password</label>
+        </div>
+        <div>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+        </div>
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
   );
 };
