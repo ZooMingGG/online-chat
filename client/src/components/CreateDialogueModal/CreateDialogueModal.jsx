9e41ffae096cd233770c07b2564db4fd3ed389f8
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2022-04-08 10:29:30 +0300

    replace basic html inputs by custom Input component in CreateDialogueModal component

--- client/src/components/CreateDialogueModal/CreateDialogueModal.jsx
+++ client/src/components/CreateDialogueModal/CreateDialogueModal.jsx
@@ -1,10 +1,21 @@
+import { useFormik } from 'formik';
 import { GiCancel } from 'react-icons/gi';
 
 import classes from './CreateDialogueModal.module.css';
 import { Button } from '../../shared/components/Button/Button';
+import { Input } from '../../shared/components/Input/Input';
 
 export const CraeteDialogueModal = ({ isVisible, setIsVisible }) => {
   const modalClasses = [classes.modal];
+  const formik = useFormik({
+    initialValues: {
+      tag: '',
+      message: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   if (isVisible) {
     modalClasses.push(classes.visible);
@@ -12,7 +23,10 @@ export const CraeteDialogueModal = ({ isVisible, setIsVisible }) => {
 
   return (
     <div className={modalClasses.join(' ')}>
-      <div className={classes['modal-container']}>
+      <form
+        onSubmit={formik.handleSubmit}
+        className={classes['modal-container']}
+      >
         <div className={classes['modal-header']}>
           <span className={classes['modal-header-text']}>
             Create Dialogue
@@ -29,7 +43,15 @@ export const CraeteDialogueModal = ({ isVisible, setIsVisible }) => {
               <label htmlFor="teg">Enter teg:</label>
             </div>
             <div>
-              <input id="teg" type="text" />
+              <Input
+                onChange={formik.handleChange}
+                value={formik.values.tag}
+                className={classes.input}
+                id="tag"
+                name="tag"
+                placeholder="Enter user tag..."
+                type="text"
+              />
             </div>
           </div>
           <div>
@@ -37,7 +59,15 @@ export const CraeteDialogueModal = ({ isVisible, setIsVisible }) => {
               <label htmlFor="message">Enter message:</label>
             </div>
             <div>
-              <input id="message" type="text" />
+              <Input
+                onChange={formik.handleChange}
+                value={formik.values.message}
+                className={classes.input}
+                id="message"
+                name="message"
+                placeholder="Enter message..."
+                type="text"
+              />
             </div>
           </div>
         </div>
@@ -51,11 +81,12 @@ export const CraeteDialogueModal = ({ isVisible, setIsVisible }) => {
           </Button>
           <Button
             className={classes['modal-btn']}
+            type="submit"
           >
             Create
           </Button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
