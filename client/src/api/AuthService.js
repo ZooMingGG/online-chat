800068a19aca514439b82b1d146ff62fd434c3fa
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2022-04-18 11:09:53 +0300

    create basic auth service

---
+++ client/src/api/AuthService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+import { API_URL } from '../constants';
+
+export class AuthService {
+  static signIn = async signInPayload => {
+    const response = await axios({
+      method: 'post',
+      url: API_URL,
+      data: signInPayload,
+    });
+
+    return response.data;
+  }
+}
