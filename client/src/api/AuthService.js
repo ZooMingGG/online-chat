33503b3d8fd503aee5ea4116b180b4e2908f5933
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2022-05-10 09:56:28 +0300

    add sign up function for auth service and change api urls

--- client/src/api/AuthService.js
+++ client/src/api/AuthService.js
@@ -6,8 +6,18 @@ export class AuthService {
   static signIn = async signInPayload => {
     const response = await axios({
       method: 'post',
-      url: API_URL,
+      url: `${API_URL}/auth/sign-in`,
       data: signInPayload,
+    });
+
+    return response.data;
+  };
+
+  static signUp = async signUpPayload => {
+    const response = await axios({
+      method: 'post',
+      url: `${API_URL}/auth/sign-up`,
+      data: signUpPayload,
     });
 
     return response.data;
